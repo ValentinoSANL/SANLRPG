76784b4cca81b3e2ee79f187f914b3067bda3b45
gSub <info@nullexception.co>
2017-04-11 12:20:32 +0400

    Added Arabic Support

--- Localisation/Client.lua
+++ Localisation/Client.lua
@@ -21,3 +21,27 @@ L10N = {
 		en_US = "Cancel"
 	}
 }
+
+Arabic = {
+	["USERNAME"] = {
+		en_US = "اسم المستخدم"
+	},
+	["PASSWORD"] = {
+		en_US = "كلمة السر"
+	},
+	["CONFIRM"] = {
+		en_US = "تأكيد"
+	},
+	["REMEMBER_PASSWORD"] = {
+		en_US = "تذكر كلمة المرور"
+	},
+	["LOGIN"] = {
+		en_US = "تسجيل الدخول"
+	},
+	["REGISTER"] = {
+		en_US = "انشاء حساب"
+	},
+	["CANCEL"] = {
+		en_US = "الغاء"
+	}
+}
