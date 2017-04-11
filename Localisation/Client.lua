5e1df6c1d228350a1bed04757c4f0c670efece1b
Callum Dawson <CallumDawson@users.noreply.github.com>
2017-04-11 04:30:34 +0100

    Added some of the client-side core strings to L10N.

--- Localisation/Client.lua
+++ Localisation/Client.lua
@@ -1,2 +1,23 @@
 L10N = {
+	["USERNAME"] = {
+		en_US = "Username:"
+	},
+	["PASSWORD"] = {
+		en_US = "Password:"
+	},
+	["CONFIRM"] = {
+		en_US = "Confirm:"
+	},
+	["REMEMBER_PASSWORD"] = {
+		en_US = "Remember my password"
+	},
+	["LOGIN"] = {
+		en_US = "Login"
+	},
+	["REGISTER"] = {
+		en_US = "Register"
+	},
+	["CANCEL"] = {
+		en_US = "Cancel"
+	}
 }
