dd7b4043ec70511e00c02c7dc687575ac5d65342
Samer Belgacem <belgacem.samer@gmail.com>
2017-04-11 10:48:24 +0200

    Update Client.lua
    Added french support.

--- Localisation/Client.lua
+++ Localisation/Client.lua
@@ -1,23 +1,30 @@
 L10N = {
 	["USERNAME"] = {
 		en_US = "Username:"
+		fr_FR = "Nom d'utilisateur:"
 	},
 	["PASSWORD"] = {
 		en_US = "Password:"
+		fr_FR = "Mot de passe:"
 	},
 	["CONFIRM"] = {
 		en_US = "Confirm:"
+		fr_FR = "Confirmer:"
 	},
 	["REMEMBER_PASSWORD"] = {
 		en_US = "Remember my password"
+		fr_FR = "Mémoriser mon mot de passe"
 	},
 	["LOGIN"] = {
 		en_US = "Login"
+		fr_FR = "Connexion"
 	},
 	["REGISTER"] = {
 		en_US = "Register"
+		fr_FR = "Créer un compte"
 	},
 	["CANCEL"] = {
 		en_US = "Cancel"
+		fr_FR = "Annuler"
 	}
 }
