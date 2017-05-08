ff4f54742a6dd43322f92142b0dca2c64a6fa081
ValentinoSANL <valentinotv123@gmail.com>
2017-05-08 20:32:15 -0300

    Update Client.lua

--- Localisation/Client.lua
+++ Localisation/Client.lua
@@ -2,36 +2,43 @@ L10N = {
 	["USERNAME"] = {
 		ar = "اسم المستخدم",
 		en_US = "Username:",
-		fr = "Nom d'utilisateur:"
+		fr = "Nom d'utilisateur:",
+		es = "Usuario:"
 	},
 	["PASSWORD"] = {
 		ar = "كلمة السر",
 		en_US = "Password:",
-		fr = "Mot de passe:"
+		fr = "Mot de passe:",
+		es = "Contraseña:"
 	},
 	["CONFIRM"] = {
 		ar = "تأكيد",
 		en_US = "Confirm:",
-		fr = "Confirmer:"
+		fr = "Confirmer:",
+		es = "Confirmar:"
 	},
 	["REMEMBER_PASSWORD"] = {
 		ar = "تذكر كلمة المرور",
 		en_US = "Remember my password",
-		fr = "Mémoriser mon mot de passe"
+		fr = "Mémoriser mon mot de passe",
+		es = "Recordar contraseña"
 	},
 	["LOGIN"] = {
 		ar = "تسجيل الدخول",
 		en_US = "Login",
-		fr = "Connexion"
+		fr = "Connexion",
+		es = "Conectar"
 	},
 	["REGISTER"] = {
 		ar = "انشاء حساب",
 		en_US = "Register",
-		fr = "Créer un compte"
+		fr = "Créer un compte",
+		es = "Resgistrarse"
 	},
 	["CANCEL"] = {
 		ar = "الغاء",
 		en_US = "Cancel",
-		fr = "Annuler"
+		fr = "Annuler",
+		es = "Cancelar"
 	}
 }
